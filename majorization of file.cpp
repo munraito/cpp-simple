1ca584485c943338deba66681487bb88bd96aaad
Vitaly <tsubasa.knight@gmail.com>
2015-05-07 17:05:43 +0300

    refactor variable names

--- majorization of file.cpp
+++ majorization of file.cpp
@@ -7,26 +7,26 @@ static const unsigned BIT_LENGTH = 8;
 
 int main(){
 	setlocale(LC_ALL, "Russian");
-	ifstream file("input.txt", ios::binary);
+	ifstream fin("input.txt", ios::binary); 
 	ofstream fout("mjr.txt", ios::binary);
 
-	if (!file.is_open()){
+	if (!fin.is_open()){
 		cout << "Файл не может быть открыт" << endl;
 		return 0;
 	}
-	file.seekg(0, file.end); //перемещаем указатель на конец файла 
-	int size = file.tellg(); //вычислили размер
-	file.seekg(0, file.beg); //возвращаем в начало
+	fin.seekg(0, fin.end); //перемещаем указатель на конец файла 
+	int size = fin.tellg(); //вычислили размер
+	fin.seekg(0, fin.beg); //возвращаем в начало
 	cout << "Размер файла: " << size << " байт\n";
 	char* bytes = new char[size];
-	file.read(bytes, size);
+	fin.read(bytes, size);
 	for (int i = 0; i < size; i++)
 	{
-		cout << "Байт "<< i << ": "<< (int*)bytes[i]<<endl;
+		cout << "Байт " << i << ": " << (int*)bytes[i] << endl;
 		fout << bytes[i] << bytes[i] << bytes[i];
 	}
 
-	file.close();
+	fin.close();
 	fout.close();
 	delete[] bytes;
 	return 0;
